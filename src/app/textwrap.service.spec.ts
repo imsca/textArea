f556a1184f252806fe94f20e420649c069e07e2e
Leonardo Ims <leonardoims@hotmail.com>
2018-03-09 11:53:01 -0300

    Feito unit tests

--- src/app/textwrap.service.spec.ts
+++ src/app/textwrap.service.spec.ts
@@ -12,4 +12,21 @@ describe('TextwrapService', () => {
   it('should be created', inject([TextwrapService], (service: TextwrapService) => {
     expect(service).toBeTruthy();
   }));
+  it('Deve fazer wrap nos textos', inject([TextwrapService], (service: TextwrapService) => {
+    let textArray: string[] = [
+     'the book is on the table',
+     'UMA PALAVRA ENORME QUE SO PODE SE CONTER EM 8 CARACTERES POR LINHAS',
+     'Lorem ipsum dolor sit amet',
+     'UMA LONGUÍSSIMA PALAVRAAAAAA QUE SERÁ CORTAAAAAADA'
+    ];
+    let arrayTest: string[][] = [
+      ["the book ", "is on ", "the ", "table"],
+      ["UMA ", "PALAVRA ", "ENORME ", "QUE SO ", "PODE SE ", "CONTER ", "EM 8 ", "CARACTER", "ES POR ", "LINHAS"],
+      ["Lorem ", "ipsum ", "dolor ", "sit amet"],
+      ["UMA ", "LONGUÍSS", "IMA ", "PALAVRAA", "AAAA QUE ", "SERÁ ", "CORTAAAA", "AADA"]
+    ];
+    for(let i = 0; i < textArray.length; i++){
+      expect(service.textWrap(textArray[i], 8)).toEqual(arrayTest[i]);
+    }
+  }));
 });
